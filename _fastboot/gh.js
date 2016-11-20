b3e74e0af930328585c1c24fc379ec80173d6baf
Nimo <nimo.jser@gmail.com>
2016-11-20 23:54:44 +0800

    fast-boot fix bug

--- _fastboot/gh.js
+++ _fastboot/gh.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var path = require('path')
 var exec = require('child_process').exec
 var rm = require('rm-r')
-var iPackage = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json').toString())
+var iPackage = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json')).toString())
 var shellPath = path.join(__dirname, 'gh.sh')
 
 require('./rm')()
