e73c2882e09b0258076b3333e64a71c880e0b4b9
Nimo <nimo.jser@gmail.com>
2016-11-20 22:26:15 +0800

    settings: need pageCount or pageSize&dataCount!

--- test/createData.js
+++ test/createData.js
@@ -1,13 +1,20 @@
 // _Package = require('../index')
 describe('#createData.js', function() {
     it('proptypes need page', function (){
-        var errorMessage = ''
-        _Package.createData({})
+        _Package.createData({pageCount: 10})
         expect(window.console._warnMsg).to.eql('Warning: Failed propType: Required prop `page` was not specified in `paging.createData`.')
     })
     it('proptypes page type need is number ', function (){
-        var errorMessage = ''
-        _Package.createData({page: '1'})
+        _Package.createData({page: '1', pageCount: 10})
         expect(window.console._warnMsg).to.eql('Warning: Failed propType: Invalid prop `page` of type `string` supplied to `paging.createData`, expected `number`.')
+    })
+    it('Error settings: need pageCount or pageSize&dataCount!', function (){
+        var errorMessage = ''
+        try {
+            _Package.createData({page: 1})
+        } catch (e) {
+            errorMessage = e.message
+        }
+        expect(errorMessage).to.eql('settings: need pageCount or pageSize&dataCount!')
     })
 })
