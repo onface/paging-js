cc2105d4fabfd6bf56868ca1564d092b3e55cd96
Nimo <nimo.jser@gmail.com>
2018-01-30 11:37:02 +0800

    del old dev

--- config/webpack.ghversion.js
+++
@@ -1,5 +0,0 @@
-var path = require('path')
-var config = require('./webpack.gh')
-var iPackage = require('../package.json')
-config.output.path = path.join(__dirname, '../output', iPackage.version)
-module.exports = config
