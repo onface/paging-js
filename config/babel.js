c7128be291019c13783c6eb2fefd5b7f1489f56a
Nimo <nimo.jser@gmail.com>
2018-01-30 22:25:48 +0800

    del old code

--- config/babel.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs')
-var path = require('path')
-var config = fs.readFileSync(path.join(__dirname, '../.babelrc')).toString()
-config = JSON.parse(config)
-module.exports = config
--- example/createData.demo.js
+++
@@ -1,8 +0,0 @@
-var paging = require('paging')
-var jsonFormat = require('json-format')
-var data = paging.createData({
-    page: 1,
-    pageSize: 10,
-    pageCount: 20,
-})
-document.getElementById('createDataNode').innerHTML = jsonFormat(data)
